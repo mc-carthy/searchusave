9d89b7c52c0cf3c0c71cc4718d75cf501ea18345
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-15 16:44:17 +0100

    Create basic advertisement list components

--- src/app/advertisements/advertisements.service.ts
+++ src/app/advertisements/advertisements.service.ts
@@ -8,9 +8,9 @@ export class AdvertisementsService {
         new Advertisement(
             'Car for sale',
             5000,
-            'Generic car description',
+            "Generic car description, this one has been drawn out a bit too much in order to show the effect of having a long description. Consider imposing a limit of description length on the advertisement listing page in order to prevent this advertisement taking up way too much room. Doesn't seem fair on the dog in the fridge below...",
             'Test contact name',
-            '0000000',
+            '02890------',
             'car@test.com',
             ['http://i.imgur.com/TPaUdg8.jpg'],
             {
@@ -22,9 +22,9 @@ export class AdvertisementsService {
         new Advertisement(
             'Fridge for sale',
             250,
-            'Generic fridge description',
+            'Generic fridge description. This one is short.',
             'Fridge seller contact name',
-            '11232332',
+            '02890------',
             'fridge@test.com',
             ['http://i.imgur.com/bkrMq.jpg'],
             {
