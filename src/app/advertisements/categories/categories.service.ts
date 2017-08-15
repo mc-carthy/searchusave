5707ded4469e4c65e17af075f272bc4e8f20e0b3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-15 18:08:05 +0100

    Show categories retrieved from service

--- src/app/advertisements/categories/categories.service.ts
+++ src/app/advertisements/categories/categories.service.ts
@@ -3,96 +3,123 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CategoriesService {
 
-    categories = {
-        'ELECTRONICS': [
-            'Home Electronics',
-            'LCDs',
-            'Mobile & Tablets',
-            'Technical Services',
-            'TV & DVDs'
-        ],
-        
-        'EVENTS & NOTICES': [
-            'Auction Announcements',
-            'Business Events',
-            'Business Notices',
-            'Children Events',
-            'Legal & Public Notices'
-        ],
-
-        'FASHION': [
-            'Bags ',
-            'Beauty Products',
-            'Clothing M/F',
-            'Jewelry',
-            'Shoes M/F'
-        ],
-
-        'HEALTH & FITNESS': [
-            'Activities',
-            'Fitness',
-            'Gym',
-            'Hospitals & Clinics',
-            'Parlours'
-        ],
-
-        'HOME & LIFESTYLE': [
-            'Art & Photography',
-            'Entertainment',
-            'Fittings',
-            'Home Furniture',
-            'Office Furniture '
-        ],
-
-        'JOBS': [
-            'Accounts Jobs',
-            'Cleaning & Washing',
-            'Data Entry',
-            'Design & Code',
-            'Finance Jobs'
-        ],
-
-        'MATRIMONIALS': [
-            'Brides',
-            'Casual Dating',
-            'Dating',
-            'Friendship',
-            'Grooms'
-        ],
-
-        'MISCELLANEOUS': [
-            'Classic & Antique',
-            'Kids Stuff',
-            'Music Equipement',
-            'Pets Stuff',
-            'Scrape'
-        ],
-
-        'REAL ESTATE': [
-            'Farms',
-            'Home for rent',
-            'Hotels',
-            'Land for sale',
-            'Offices for rent'
-        ],
-
-        'SERVICES': [
-            'Cleaning Services',
-            'Educational',
-            'Food Services',
-            'Medical',
-            'Office & Home Removals'
-        ],
-
-        'VEHICLES': [
-            'Cars',
-            'Classic & Modern',
-            'Fancy Cars',
-            'Kids Bikes',
-            'Motor Bikes'
-        ]
-    }
+    categories = [
+        {
+            name: 'Electronics',
+            values: [
+                'Home Electronics',
+                'LCDs',
+                'Mobile & Tablets',
+                'Technical Services',
+                'TV & DVDs'
+            ],
+        },
+        {
+            name: 'Events & Notices',
+            values: [
+                'Auction Announcements',
+                'Business Events',
+                'Business Notices',
+                'Children Events',
+                'Legal & Public Notices'
+            ],
+        },
+        {
+            name: 'Fashion',
+            values: [
+                'Bags ',
+                'Beauty Products',
+                'Clothing M/F',
+                'Jewelry',
+                'Shoes M/F'
+            ],
+        },
+        {
+            name: 'Health & Fitness',
+            values: [
+                'Activities',
+                'Fitness',
+                'Gym',
+                'Hospitals & Clinics',
+                'Parlours'
+            ],
+        },
+        {
+            name: 'Home & Lifestyle',
+            values: [
+                'Art & Photography',
+                'Entertainment',
+                'Fittings',
+                'Home Furniture',
+                'Office Furniture '
+            ],
+        },
+        {
+            name: 'Jobs',
+            values: [
+                'Accounts Jobs',
+                'Cleaning & Washing',
+                'Data Entry',
+                'Design & Code',
+                'Finance Jobs'
+            ],
+        },
+        {
+            name: 'Matrimonials',
+            values: [
+                'Brides',
+                'Casual Dating',
+                'Dating',
+                'Friendship',
+                'Grooms'
+            ],
+        },
+        {
+            name: 'Miscellaneous',
+            values: [
+                'Classic & Antique',
+                'Kids Stuff',
+                'Music Equipement',
+                'Pets Stuff',
+                'Scrape'
+            ],
+        },
+        {
+            name: 'Real Estate',
+            values: [
+                'Farms',
+                'Home for rent',
+                'Hotels',
+                'Land for sale',
+                'Offices for rent'
+            ],
+        },
+        {
+            name: 'Services',
+            values: [
+                'Cleaning Services',
+                'Educational',
+                'Food Services',
+                'Medical',
+                'Office & Home Removals'
+            ],
+        },
+        {
+            name: 'Vehicles',
+            values: [
+                'Cars',
+                'Classic & Modern',
+                'Fancy Cars',
+                'Kids Bikes',
+                'Motor Bikes'
+            ],
+        },
+    ]
 
     constructor() { }
+
+    getCategories() {
+        return this.categories;
+    }
 
 }
--- src/app/advertisements/categories/view-categories/view-categories.component.ts
+++ src/app/advertisements/categories/view-categories/view-categories.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from './../categories.service';
 
 @Component({
-  selector: 'app-view-categories',
-  templateUrl: './view-categories.component.html',
-  styleUrls: ['./view-categories.component.css']
+    selector: 'app-view-categories',
+    templateUrl: './view-categories.component.html',
+    styleUrls: ['./view-categories.component.css']
 })
 export class ViewCategoriesComponent implements OnInit {
 
-  constructor() { }
+    categories;
 
-  ngOnInit() {
-  }
+    constructor(private categoriesService: CategoriesService) { }
+
+    ngOnInit() {
+        this.categories = this.categoriesService.getCategories();
+        console.log(this.categories);
+    }
 
 }
